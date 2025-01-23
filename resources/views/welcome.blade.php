5179b4da0673776545e2bcdbf078c4c8d2ceebfa
Faishal Rahman <neracaiddeveloper@gmail.com>
2025-01-23 15:50:00 +0700

    tes perubahan gw

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,10 +94,10 @@
                                         </div>
 
                                         <div class="pt-3 sm:pt-5 lg:pt-0">
-                                            <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
+                                            <h2 class="text-xl font-semibold text-black dark:text-white">muhashaall17_</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                coba test perubahan branch gw ini ajgg. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, hic. Atque delectus, voluptatum quia quibusdam quo fuga ad distinctio ea sunt harum expedita error hic laboriosam aperiam ducimus odio! Deserunt?
                                             </p>
                                         </div>
                                     </div>
